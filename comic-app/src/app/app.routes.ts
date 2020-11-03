84a333d01586d9b28d8ec3d4060965de24ea5c88
g5sh <eagomez2387@gmail.com>
2020-11-03 18:48:49 -0500

    [FIX]: Fix hoe rutes

--- comic-app/src/app/app.routes.ts
+++ comic-app/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { AboutComponent  } from '../app/components/about/about.component';
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: '**', pathMatch: 'full', redirectTo: '' }
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
